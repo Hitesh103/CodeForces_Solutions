529de7b0b6900d05b2af6c234eff1954027fb138
Hitesh <hp5741609@gmail.com>
2023-08-19 15:52:03 +0530

    A wrong Substraction

--- A_Wrong_Subtraction.cpp
+++ A_Wrong_Subtraction.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 
 int main(){
-    
+    int t;
+    int num;
+
+    cin  >> num >> t;
+
+    while(t--){
+        if(num%10 == 0){
+            num = num / 10;
+        }else{
+            num--;
+        }
+    }
+
+    cout << num;
+
     return 0;
 }
